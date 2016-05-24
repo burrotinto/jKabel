bf1359d39cc293efe9d838d13cf3a63540b13e69
Florian Klinger <derduke@posteo.de>
2016-05-24 09:38:08 +0200

    Pfad angepasst

--- src/de/swneumarkt/jKabeltrommel/dbauswahlAS/DBAuswahlAAS.java
+++ src/de/swneumarkt/jKabeltrommel/dbauswahlAS/DBAuswahlAAS.java
@@ -29,8 +29,8 @@ public class DBAuswahlAAS {
 
     public IDBWrapper getDBWrapper() {
         try {
-            if(new File("O:KFM-VerwaltungMaterialwirtschaftLagerjKabelDB").exists()){
-                return new HSQLDBWrapper("O:KFM-VerwaltungMaterialwirtschaftLagerjKabelDB");
+            if(new File("O:\\KFM-Verwaltung\\Materialwirtschaft\\Lager\\jKabelDB").exists()){
+                return new HSQLDBWrapper("O:\\KFM-Verwaltung\\Materialwirtschaft\\Lager\\jKabelDB");
             } else {
                 return new HSQLDBWrapper(getPath());
             }
